3f5c4cce3be16251ec11141b1873d230f999aea9
Bhabani Shankar Behera <beherabhabani610@gmail.com>
2026-02-20 16:50:08 +0530

    Python Folder

--- OOPS/OOPS2.py
+++ OOPS/OOPS2.py
@@ -126,3 +126,28 @@ r1=Rectangle()
 r1.show()
 r1.area()
 print("perimeter of rectangle=",r1.perimeter())
+
+
+
+
+
+
+class Rectangle:
+	def __init__(self,L,B):   #parameter constrcutor
+		self.length=L
+		self.breadth=B
+	def show(self):
+		print("rectangle lenegth=",self.length)
+		print("rectangle breadth=",self.breadth)
+	def area(self):
+		print("area of rectangle=",self.length*self.breadth)
+	def perimeter(self):
+		return 2*(self.length+self.breadth)
+r=Rectangle(5,7)  #object1 
+r.show()
+r.area()
+print("perimeter of rectangle=",r.perimeter())
+r1=Rectangle(8,9)  # object2
+r1.show()
+r1.area()
+print("perimeter of rectangle=",r1.perimeter())
